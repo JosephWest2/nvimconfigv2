899a0ad7c726185a6a1e39665eb591c10705455a
JosephWest2 <joeywest1414@gmail.com>
2024-11-30 16:03:46 -0700

    added harpoon

--- init.lua
+++ init.lua
@@ -1,4 +1,4 @@
-vim.o.shiftwidth = 4
+vim.o.shiftwidth = 10
 vim.o.tabstop = 4
 vim.o.softtabstop = 4
 vim.o.expandtab = true
@@ -44,6 +44,11 @@ require("lazy").setup({
     {
         'nvim-tree/nvim-tree.lua',
         dependencies = { 'nvim-tree/nvim-web-devicons' },
+    },
+    {
+        "ThePrimeagen/harpoon",
+        branch = "harpoon2",
+        dependencies = { "nvim-lua/plenary.nvim" }
     },
 
     -- lsp, snippets, autocomplete
@@ -104,7 +109,6 @@ vim.api.nvim_create_autocmd('LspAttach', {
 })
 
 local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
-
 local default_setup = function(server)
     require('lspconfig')[server].setup({
         capabilities = lsp_capabilities,
@@ -153,6 +157,7 @@ cmp.setup({
     },
 })
 
+-- Treesitter
 require('nvim-treesitter.configs').setup {
     sync_install = false,
     auto_install = true,
@@ -162,12 +167,15 @@ require('nvim-treesitter.configs').setup {
     }
 }
 
+-- Telescope
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
 
+
+-- Nvim tree
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 vim.keymap.set('n', '<Leader>t', "<cmd>silent NvimTreeOpen<cr>")
@@ -183,6 +191,7 @@ require('nvim-tree').setup {
     end
 }
 
+-- LSP Signature
 require('lsp_signature').setup({
     extra_trigger_chars = {' '},
     hint_enable = false,
@@ -190,3 +199,36 @@ require('lsp_signature').setup({
         require('lsp_signature').toggle_float_win()
     end, { silent = true, noremap = true, desc = 'toggle lsp signature window' })
 });
+
+-- Harpoon
+local harpoon = require("harpoon")
+harpoon:setup()
+vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
+vim.keymap.set("n", "<leader>r", function() harpoon:list():remove() end)
+
+vim.keymap.set("n", "<C-1>", function() harpoon:list():select(1) end)
+vim.keymap.set("n", "<C-2>", function() harpoon:list():select(2) end)
+vim.keymap.set("n", "<C-3>", function() harpoon:list():select(3) end)
+vim.keymap.set("n", "<C-4>", function() harpoon:list():select(4) end)
+
+vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
+vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
+local conf = require("telescope.config").values
+local function toggle_telescope(harpoon_files)
+    local file_paths = {}
+    for _, item in ipairs(harpoon_files.items) do
+        table.insert(file_paths, item.value)
+    end
+
+    require("telescope.pickers").new({}, {
+        prompt_title = "Harpoon",
+        finder = require("telescope.finders").new_table({
+            results = file_paths,
+        }),
+        previewer = conf.file_previewer({}),
+        sorter = conf.generic_sorter({}),
+    }):find()
+end
+
+vim.keymap.set("n", "<C-h>", function() toggle_telescope(harpoon:list()) end,
+    { desc = "Open harpoon window" })
