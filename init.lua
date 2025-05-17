e19b821c07d90c384d3dcd1e7b72452202576f18
JosephWest2 <joeywest1414@gmail.com>
2025-05-16 20:20:52 -0600

    updated to .11 lsp

--- init.lua
+++ init.lua
@@ -1,4 +1,7 @@
+vim.g.mapleader = " "
+
 vim.o.shiftwidth = 4
+
 vim.o.tabstop = 4
 vim.o.softtabstop = 4
 vim.o.expandtab = true
@@ -11,12 +14,21 @@ vim.o.rnu = true
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
-vim.keymap.set("n", "<leader>y", "\"+y")
-vim.keymap.set("v", "<leader>y", "\"+y")
-vim.keymap.set("n", "<leader>p", "\"+p")
-vim.keymap.set("v", "<leader>p", "\"+p")
-vim.keymap.set("n", "<leader>P", "\"+P")
-vim.keymap.set("v", "<leader>P", "\"+P")
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+vim.keymap.set({ "n", "v" }, "<leader>p", [["+p]])
+vim.keymap.set({ "n", "v" }, "<leader>P", [["+P]])
+vim.keymap.set({ "n", "v" }, "<A-y>", [["*y]])
+vim.keymap.set({ "n", "v" }, "<A-p>", [["*p]])
+vim.keymap.set({ "n", "v" }, "<A-P>", [["*P]])
+
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/g<Left><Left>]])
+
+vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]])
+vim.keymap.set("t", "<C-[>", [[<C-\><C-n>]])
+vim.keymap.set("t", "C-c", [[<C-\><C-n>]])
+
+vim.keymap.set("n", "-", "<cmd>Oil<CR>")
+vim.keymap.set({ "i", "n", "v" }, "<C-c>", "<Esc>")
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
@@ -52,11 +64,38 @@ require("lazy").setup({
             'nvim-tree/nvim-web-devicons',
         },
     },
+    {
+        'rmagatti/auto-session',
+        lazy = false,
+        ---@module "auto-session"
+        ---@type AutoSession.Config
+        opts = {
+            suppressed_dirs = { '~/', '~/Downloads', '/', '~/development' }
+        }
+    },
+    {
+        'stevearc/oil.nvim',
+        ---@module 'oil'
+        ---@type oil.SetupOpts
+        opts = {},
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+        lazy = false,
+    },
 
-    -- lsp, snippets, autocomplete
-    { 'neovim/nvim-lspconfig' },
-    { 'williamboman/mason.nvim' },
-    { 'williamboman/mason-lspconfig.nvim' },
+    -- lsp stuff
+    {
+        "mason-org/mason.nvim",
+        config = function()
+            require('mason').setup()
+        end
+    },
+    {
+        'neovim/nvim-lspconfig',
+        config = function()
+            require('lspconfig').lua_ls.setup {}
+            require('lspconfig').clangd.setup {}
+        end
+    },
     { 'hrsh7th/nvim-cmp' },
     { 'hrsh7th/cmp-nvim-lsp' },
     {
@@ -67,76 +106,8 @@ require("lazy").setup({
     },
     { 'saadparwaiz1/cmp_luasnip' },
     { 'ray-x/lsp_signature.nvim' },
-    { 'Civitasv/cmake-tools.nvim' },
-
+    { 'Civitasv/cmake-tools.nvim' }
 }, {})
-
-
-require('onedark').setup {
-    style = 'cool',
-    colors = {
-        bg0 = '#21262f',
-    },
-}
-require('onedark').load()
-
-require('autoclose').setup({
-    keys = {
-        ["("] = { escape = false, close = true, pair = "()" },
-        ["["] = { escape = false, close = true, pair = "[]" },
-        ["{"] = { escape = false, close = true, pair = "{}" },
-        [")"] = { escape = true, close = false, pair = "()" },
-        ["]"] = { escape = true, close = false, pair = "[]" },
-        ["}"] = { escape = true, close = false, pair = "{}" },
-        ['"'] = { escape = true, close = true, pair = '""' },
-        ["'"] = { escape = true, close = false, pair = "''" },
-        ["`"] = { escape = true, close = true, pair = "``" },
-    },
-    options = {
-        auto_indent = true,
-    },
-})
-
-vim.keymap.set("n", "<Leader>e", "<cmd>lua vim.diagnostic.open_float()<cr>")
-vim.keymap.set("n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<cr>")
-vim.keymap.set("n", "]d", "<cmd>lua vim.diagnostic.goto_next()<cr>")
-
-vim.api.nvim_create_autocmd('LspAttach', {
-    desc = 'LSP actions',
-    callback = function(event)
-        local opts = { buffer = event.buf }
-        vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
-        vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
-        vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
-        vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
-        vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
-        vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
-        vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
-        vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
-        vim.keymap.set({ 'n', 'x' }, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
-        vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
-    end
-})
-
-local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
-local default_setup = function(server)
-    require('lspconfig')[server].setup({
-        capabilities = lsp_capabilities,
-    })
-end
-
-require('mason').setup({})
-require('mason-lspconfig').setup({
-    handlers = {
-        default_setup,
-        ["rust_analyzer"] = function()
-            require('lspconfig').rust_analyzer.setup({
-                capabilities = lsp_capabilities,
-            })
-        end
-    },
-})
-vim.g.zig_fmt_autosave = 0
 
 require("luasnip.loaders.from_vscode").lazy_load()
 
@@ -189,6 +160,61 @@ cmp.setup.cmdline(':', {
     matching = { disallow_symbol_nonprefix_matching = false }
 })
 
+-- LSP Signature
+require('lsp_signature').setup({
+    extra_trigger_chars = { ' ' },
+    hint_enable = false,
+    vim.keymap.set({ 'n', 'i', 'v' }, '<C-s>', function()
+        require('lsp_signature').toggle_float_win()
+    end, { silent = true, noremap = true, desc = 'toggle lsp signature window' })
+})
+
+require('onedark').setup {
+    style = 'cool',
+    colors = {
+        bg0 = '#21262f',
+    },
+}
+require('onedark').load()
+
+require('autoclose').setup({
+    keys = {
+        ["("] = { escape = false, close = true, pair = "()" },
+        ["["] = { escape = false, close = true, pair = "[]" },
+        ["{"] = { escape = false, close = true, pair = "{}" },
+        [")"] = { escape = true, close = false, pair = "()" },
+        ["]"] = { escape = true, close = false, pair = "[]" },
+        ["}"] = { escape = true, close = false, pair = "{}" },
+        ['"'] = { escape = true, close = true, pair = '""' },
+        ["'"] = { escape = true, close = false, pair = "''" },
+        ["`"] = { escape = true, close = true, pair = "``" },
+    },
+    options = {
+        auto_indent = true,
+    },
+})
+
+vim.keymap.set("n", "<Leader>e", "<cmd>lua vim.diagnostic.open_float()<cr>")
+vim.keymap.set("n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<cr>")
+vim.keymap.set("n", "]d", "<cmd>lua vim.diagnostic.goto_next()<cr>")
+
+vim.api.nvim_create_autocmd('LspAttach', {
+    desc = 'LSP actions',
+    callback = function(event)
+        local opts = { buffer = event.buf }
+        vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
+        vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+        vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+        vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+        vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
+        vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
+        vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+        vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+        vim.keymap.set({ 'n', 'x' }, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
+        vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+    end
+})
+
 -- Treesitter
 require('nvim-treesitter.configs').setup {
     sync_install = false,
@@ -201,7 +227,10 @@ require('nvim-treesitter.configs').setup {
 
 -- Telescope
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>ff',
+    "<cmd>lua require'telescope.builtin'.find_files({ find_command = {'rg', '--files', '--hidden', '-g', '!.git' }})<cr>",
+    { desc = 'Telescope find files' })
+--vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
@@ -224,17 +253,48 @@ require('nvim-tree').setup {
     end
 }
 
--- LSP Signature
-require('lsp_signature').setup({
-    extra_trigger_chars = { ' ' },
-    hint_enable = false,
-    vim.keymap.set({ 'n', 'i', 'v' }, '<C-s>', function()
-        require('lsp_signature').toggle_float_win()
-    end, { silent = true, noremap = true, desc = 'toggle lsp signature window' })
-});
-
 -- BarBar
 vim.keymap.set('n', '<A-p>', "<cmd>silent BufferPrevious<CR>")
 vim.keymap.set('n', '<A-n>', "<cmd>silent BufferNext<CR>")
 vim.keymap.set('n', '<C-p>', "<cmd>BufferPick<CR>")
 vim.keymap.set("n", "<A-q>", "<cmd>BufferClose<CR>")
+
+-- auto save
+require("auto-save").setup {
+    enabled = true,          -- start auto-save when the plugin is loaded (i.e. when your package manager loads it)
+    execution_message = {
+        message = function() -- message to print on save
+            return ("AutoSave: saved at " .. vim.fn.strftime("%H:%M:%S") .. " filetype: " .. vim.bo.filetype)
+        end,
+        dim = 0.18,                     -- dim the color of `message`
+        cleaning_interval = 1250,       -- (milliseconds) automatically clean MsgArea after displaying `message`. See :h MsgArea
+    },
+    trigger_events = { "InsertLeave" }, -- vim events that trigger auto-save. See :h events
+    -- function that determines whether to save the current buffer or not
+    -- return true: if buffer is ok to be saved
+    -- return false: if it's not ok to be saved
+    condition = function(buf)
+        local fn = vim.fn
+        local utils = require("auto-save.utils.data")
+        if
+            vim.bo.filetype == "TelescopePrompt" or
+            vim.bo.filetype == "oil" then
+            return false
+        end
+        if
+            fn.getbufvar(buf, "&modifiable") == 1 and
+            utils.not_in(fn.getbufvar(buf, "&filetype"), {}) then
+            return true              -- met condition(s), can save
+        end
+        return false                 -- can't save
+    end,
+    write_all_buffers = false,       -- write all buffers when the current one meets `condition`
+    debounce_delay = 135,            -- saves the file at most every `debounce_delay` milliseconds
+    callbacks = {                    -- functions to be executed at different intervals
+        enabling = nil,              -- ran when enabling auto-save
+        disabling = nil,             -- ran when disabling auto-save
+        before_asserting_save = nil, -- ran before checking `condition`
+        before_saving = nil,         -- ran before doing the actual save
+        after_saving = nil           -- ran after doing the actual save
+    }
+}
